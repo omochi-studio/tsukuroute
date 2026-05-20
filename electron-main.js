5ef3ab2f27efc2ccb08b699fb5fd00a82714ce19
omochi-studio <serveradmin112554@gmail.com>
2026-05-20 16:26:12 +0900

    electron導入
    electronの導入は完了
    step4はまだ未実装

---
+++ electron-main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1000,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "out", "index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
